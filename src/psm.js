3a048e2a5b6460dde69aaa05d519b65c0f681eca
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-11 15:34:22 +0100

    Update psm.js

--- src/psm.js
+++ src/psm.js
@@ -1,4 +1,13 @@
-let assem = ast => {
+const token = {
+  white: /^\s*[\r\n]+|#.*/gm,
+  name: /^([a-zA-Z_][\w_]*):/,
+  nend: /:[\n\s]+/g,
+  data: /(".*"|\d+)/g,
+  code: /^[a-z]+\s([a-d]|\d+)(,[a-d]|\d+)*$/
+}
+
+
+let assemble = ast => {
 	
 }
 

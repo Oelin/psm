7816cedd774f20c30842742fa3cc06c46438a843
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-11 15:36:33 +0100

    Update parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,1 +1,19 @@
+let array = line => {
+  let m
 
+  return (
+    m = [...line.matchAll(token.data)]
+    .map(e => JSON.parse(e[0]))
+  )
+  .length && m
+}
+
+
+let code = line => {
+
+}
+
+
+let parse = list =>
+  list.map(e => e.match(token.code) ?
+  code(e) : array(e))

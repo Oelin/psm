907feecfeb2d6cbef2114c201ef5f791130fe628
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-11 12:05:50 +0100

    Update pure.js

--- src/pure.js
+++ src/pure.js
@@ -1,1 +1,29 @@
+let white = /^\s*[\r\n]+|#.*/gm
+let name = /^([a-zA-Z_][\w_]*):/
+let nend = /:[\n\s]+/g
 
+
+let prune = src =>
+  src.replaceAll(white, '')
+  .replaceAll(nend, ':').trim()
+
+
+let names = list => {
+  let m
+
+  return list.map(e =>
+    (m = e.match(name)) && m[1])
+}
+
+
+let pure = src => {
+  src = prune(src)
+  let list = src.split('\n')
+
+  names(list).forEach((name, n) => {
+    src = src.replaceAll(`$${name}`, n)
+    .replaceAll(`${name}:`, '')
+  })
+
+  return src
+}

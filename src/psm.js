3e04ef58cca9928268de515026cabac7f6e4ea4a
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-11 15:55:16 +0100

    Update psm.js

--- src/psm.js
+++ src/psm.js
@@ -1,3 +1,23 @@
+const codes = [
+  'add reg reg',
+  'add reg val',
+  'sub reg reg',
+  'sub reg val',
+  'and reg reg',
+  'or reg reg',
+  'xor reg reg',
+  'not reg',
+  'b val',
+  'mov reg reg',
+  'mov reg val',
+  'load reg val',
+  'sto reg reg,
+  'sto val reg',
+  'test reg reg',
+  'stop'
+]
+
+
 const token = {
   white: /^\s*[\r\n]+|#.*/gm,
   name: /^([a-zA-Z_][\w_]*):/,

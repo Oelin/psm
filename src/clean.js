48d85f710ba0313923099c24c774893bd3e4b1ab
Oelin <42823429+Oelin@users.noreply.github.com>
2020-10-01 15:43:33 +0100

    Update and rename pure.js to clean.js

--- src/clean.js
+++ src/clean.js
@@ -1,18 +1,18 @@
-let prune = src =>
-  src.replaceAll(token.white, '')
-  .replaceAll(token.nend, ':').trim()
+let trim = src =>
+  src.replaceAll(tok.white, '')
+  .replaceAll(tok.nend, ':').trim()
 
 
 let names = list => {
   let m
 
   return list.map(e =>
-    (m = e.match(token.name)) && m[1])
+    (m = e.match(tok.name)) && m[1])
 }
 
 
-let pure = src => {
-  src = prune(src)
+let clean = src => {
+  src = trim(src)
   let list = src.split('\n')
 
   names(list).forEach((name, n) => {

3c006d3795dc01b1c06dac04551d88bd1be0c043
Oelin <42823429+Oelin@users.noreply.github.com>
2020-10-03 18:26:53 +0100

    Update and rename clean.js to strip.js

--- src/strip.js
+++ src/strip.js
@@ -1,26 +1,21 @@
-let white = /^\s*[\r\n]+|#.*/gm
-let name = /^([a-zA-Z_][\w_]*):/
-let eon = /:[\n\s]+/g
-
-
 let trim = src =>
   src.replaceAll(white, '')
   .replaceAll(eon, ':').trim()
 
 
-let names = list => {
+let sub = list => {
   let m
-
+	
   return list.map(e =>
   (m = e.match(name)) && m[1])
 }
 
 
-let clean = src => {
+let strip = src => {
   src = trim(src)
   let list = src.split('\n')
 
-  names(list).forEach((name, n) => {
+  sub(list).forEach((name, n) => {
     src = src.replaceAll(`$${name}`, n)
     .replaceAll(`${name}:`, '')
   })

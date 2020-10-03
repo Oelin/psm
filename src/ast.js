7682888a34b872fe2bd9d6b5bc5e91f56640276a
Oelin <42823429+Oelin@users.noreply.github.com>
2020-10-03 12:40:50 +0100

    Update ast.js

--- src/ast.js
+++ src/ast.js
@@ -1,17 +1,27 @@
-let op = /([abd-y]+)([zc])?/
-let reg = /[a-z]/g
 let elem = /("[^"]*"|\d+)/g
 let loc = /a line of code/
 
 
-let array = line =>
-  [...line.matchAll(elem)].map(e => JSON.parse(e[0]))
+// parses an array of data values 
 
+let array = line =>
+  [...line.matchAll(elem)]
+  .map(e => JSON.parse(e[0]))
+
+
+let cut = line =>
+  line.replaceall(',', '').replaceAll(/\s+/g, ' ')
+  .split(' ')
+
+
+// parses a line of assembly code
 
 let code = line => {
-
+	let parts = cut(line)
+	
+	return parts
 }
 
 
-let ast = list =>
+let parse = list =>
   list.map(e => e.match(loc) ? code(e) : array(e))
